832289a615d6f8894f1d41112150a068c94e0650
jshong <jshong@iotree.co.kr>
2021-12-02 11:23:02 +0900

    달력

--- app/src/main/java/kr/co/iotree/todolist/viewholder/CalendarViewHolder.kt
+++ app/src/main/java/kr/co/iotree/todolist/viewholder/CalendarViewHolder.kt
@@ -20,27 +20,26 @@ import kr.co.iotree.todolist.util.getToday
 import kr.co.iotree.todolist.util.setImageViewColor
 
 class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : RecyclerView.ViewHolder(binding.root) {
-    lateinit var clickedDate: String
+    var selectedDate: Int = 0
     var year = getToday("yyyy").toInt()
     var month = getToday("MM").toInt()
     var isMonth = true
 
     fun bindData() {
         setYearMonth()
-        setMonthCalendar(year, month)
+        setMonthCalendar(year, month) //처음엔 오늘날짜로
         setClickListener()
     }
 
     private fun setYearMonth() {
         binding.yearMonth.text = getToday("yyyy년 MM월")
-        clickedDate = getToday("d")
+        selectedDate = getToday("d").toInt()
     }
 
 
     /**
      * 달력 세팅
      */
-    //TODO 주 달라질 때 마진
     private fun setMonthCalendar(year: Int, month: Int) {
         val iconList = mutableListOf<ImageView>()
         val dateList = mutableListOf<TextView>()
@@ -61,6 +60,9 @@ class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : Recyc
             }
             else -> 0
         }
+        var selectedIndex = 0
+
+        setBasicViewVisibility()
 
         //view 배열에 추가
         for (i in 0 until 42) {
@@ -73,48 +75,62 @@ class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : Recyc
             }
             if (i < dayOfWeek || dateTag > maxDate) {
                 icon.visibility = View.INVISIBLE
+                icon.isEnabled = false
             } else {
                 icon.tag = dateTag
+            }
+            icon.setOnClickListener {
+                dateList[selectedIndex].setTextColor(Color.parseColor("#808080"))
+                dateList[selectedIndex].setTypeface(null, Typeface.NORMAL)
+                dateList[selectedIndex].paintFlags = 0
+
+                dateList[i].setTextColor(Color.parseColor("#000000"))
+                dateList[i].setTypeface(null, Typeface.BOLD)
+                dateList[i].paintFlags = Paint.UNDERLINE_TEXT_FLAG
+
+                selectedDate = it.tag.toString().toInt()
+                selectedIndex = i
             }
             iconList.add(icon)
 
             //달력 텍스트
-            val text = TextView(itemView.context)
-            text.text = i.toString()
-            if (dateTag == clickedDate.toInt()) {
-                text.setTextColor(Color.parseColor("#000000"))
-                text.setTypeface(null, Typeface.BOLD)
-                text.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+            val date = TextView(itemView.context)
+            date.text = i.toString()
+            if (dateTag == selectedDate) {
+                date.setTextColor(Color.parseColor("#000000"))
+                date.setTypeface(null, Typeface.BOLD)
+                date.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+
+                selectedIndex = i
             }
-            text.layoutParams = calendarDateParam
-            text.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemView.context.resources.getDimension(R.dimen.calendar_date_font_size))
-            text.gravity = Gravity.CENTER
+            date.layoutParams = calendarDateParam
+            date.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemView.context.resources.getDimension(R.dimen.calendar_date_font_size))
+            date.gravity = Gravity.CENTER
 
             if (i < dayOfWeek || dateTag > maxDate) {
-                text.visibility = View.INVISIBLE
+                date.visibility = View.INVISIBLE
+                date.isEnabled = false
             } else {
-                icon.tag = dateTag
-                text.text = dateTag.toString()
+                date.tag = dateTag
+                date.text = dateTag.toString()
                 dateTag++
             }
 
-            dateList.add(text)
+            date.setOnClickListener {
+                dateList[selectedIndex].setTextColor(Color.parseColor("#808080"))
+                dateList[selectedIndex].setTypeface(null, Typeface.NORMAL)
+                dateList[selectedIndex].paintFlags = 0
+
+                (it as TextView).setTextColor(Color.parseColor("#000000"))
+                it.setTypeface(null, Typeface.BOLD)
+                it.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+
+                selectedDate = it.tag.toString().toInt()
+                selectedIndex = i
+            }
+
+            dateList.add(date)
         }
-
-        //view 지우기
-        binding.week1ImageContainer.removeAllViews()
-        binding.week2ImageContainer.removeAllViews()
-        binding.week3ImageContainer.removeAllViews()
-        binding.week4ImageContainer.removeAllViews()
-        binding.week5ImageContainer.removeAllViews()
-        binding.week6ImageContainer.removeAllViews()
-
-        binding.week1TextContainer.removeAllViews()
-        binding.week2TextContainer.removeAllViews()
-        binding.week3TextContainer.removeAllViews()
-        binding.week4TextContainer.removeAllViews()
-        binding.week5TextContainer.removeAllViews()
-        binding.week6TextContainer.removeAllViews()
 
         //view 추가
         for (i in 0 until 7) {
@@ -122,24 +138,85 @@ class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : Recyc
             binding.week2ImageContainer.addView(iconList[i + 7])
             binding.week3ImageContainer.addView(iconList[i + 14])
             binding.week4ImageContainer.addView(iconList[i + 21])
-            if (iconList[28].visibility == View.VISIBLE)
+            if (iconList[28].visibility == View.VISIBLE) {
+                binding.week5ImageContainer.visibility = View.VISIBLE
                 binding.week5ImageContainer.addView(iconList[i + 28])
-            if (iconList[35].visibility == View.VISIBLE)
+            } else {
+                binding.week5ImageContainer.visibility = View.GONE
+            }
+            if (iconList[35].visibility == View.VISIBLE) {
+                binding.week6ImageContainer.visibility = View.VISIBLE
                 binding.week6ImageContainer.addView(iconList[i + 35])
+            } else {
+                binding.week6ImageContainer.visibility = View.GONE
+            }
 
             binding.week1TextContainer.addView(dateList[i])
             binding.week2TextContainer.addView(dateList[i + 7])
             binding.week3TextContainer.addView(dateList[i + 14])
             binding.week4TextContainer.addView(dateList[i + 21])
-            if (iconList[28].visibility == View.VISIBLE)
+            if (iconList[28].visibility == View.VISIBLE) {
+                binding.week5TextContainer.visibility = View.VISIBLE
                 binding.week5TextContainer.addView(dateList[i + 28])
-            if (iconList[35].visibility == View.VISIBLE)
+            } else {
+                binding.week5TextContainer.visibility = View.GONE
+            }
+            if (iconList[35].visibility == View.VISIBLE) {
+
+                binding.week6TextContainer.visibility = View.VISIBLE
                 binding.week6TextContainer.addView(dateList[i + 35])
+            } else {
+                binding.week6TextContainer.visibility = View.GONE
+            }
         }
     }
 
     private fun setWeekCalendar(year: Int, month: Int) {
+        setBasicViewVisibility()
+        selectedDate
+    }
 
+    private fun setBasicViewVisibility() {
+        if (isMonth) { //month 모든 줄 보이게
+            binding.week2ImageContainer.visibility = View.VISIBLE
+            binding.week2TextContainer.visibility = View.VISIBLE
+            binding.week3ImageContainer.visibility = View.VISIBLE
+            binding.week3TextContainer.visibility = View.VISIBLE
+            binding.week4ImageContainer.visibility = View.VISIBLE
+            binding.week4TextContainer.visibility = View.VISIBLE
+            binding.week5ImageContainer.visibility = View.VISIBLE
+            binding.week5TextContainer.visibility = View.VISIBLE
+            binding.week6ImageContainer.visibility = View.VISIBLE
+            binding.week6TextContainer.visibility = View.VISIBLE
+
+            binding.week1ImageContainer.removeAllViews()
+            binding.week2ImageContainer.removeAllViews()
+            binding.week3ImageContainer.removeAllViews()
+            binding.week4ImageContainer.removeAllViews()
+            binding.week5ImageContainer.removeAllViews()
+            binding.week6ImageContainer.removeAllViews()
+
+            binding.week1TextContainer.removeAllViews()
+            binding.week2TextContainer.removeAllViews()
+            binding.week3TextContainer.removeAllViews()
+            binding.week4TextContainer.removeAllViews()
+            binding.week5TextContainer.removeAllViews()
+            binding.week6TextContainer.removeAllViews()
+        } else { //week 한줄만
+            binding.week2ImageContainer.visibility = View.GONE
+            binding.week2TextContainer.visibility = View.GONE
+            binding.week3ImageContainer.visibility = View.GONE
+            binding.week3TextContainer.visibility = View.GONE
+            binding.week4ImageContainer.visibility = View.GONE
+            binding.week4TextContainer.visibility = View.GONE
+            binding.week5ImageContainer.visibility = View.GONE
+            binding.week5TextContainer.visibility = View.GONE
+            binding.week6ImageContainer.visibility = View.GONE
+            binding.week6TextContainer.visibility = View.GONE
+
+            binding.week1ImageContainer.removeAllViews()
+            binding.week1TextContainer.removeAllViews()
+        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -151,12 +228,12 @@ class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : Recyc
                 year--
             }
 
-            binding.yearMonth.text = "${year}년 ${month}월"
-
-            if (isMonth)
+            if (isMonth) {
                 setMonthCalendar(year, month)
-            else
+                binding.yearMonth.text = "${year}년 ${month}월"
+            } else {
                 setWeekCalendar(year, month)
+            }
         }
 
         binding.next.setOnClickListener {
@@ -166,12 +243,12 @@ class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : Recyc
                 year++
             }
 
-            binding.yearMonth.text = "${year}년 ${month}월"
-
-            if (isMonth)
+            if (isMonth) {
                 setMonthCalendar(year, month)
-            else
+                binding.yearMonth.text = "${year}년 ${month}월"
+            } else {
                 setWeekCalendar(year, month)
+            }
         }
 
         binding.monthWeekContainer.setOnClickListener {
@@ -179,10 +256,12 @@ class CalendarViewHolder(private val binding: ViewholderCalendarBinding) : Recyc
                 binding.arrow.setImageResource(R.drawable.ic_calender_down)
                 binding.monthWeek.text = "주"
                 isMonth = !isMonth
+                setWeekCalendar(year, month)
             } else {
                 binding.arrow.setImageResource(R.drawable.ic_calender_up)
                 binding.monthWeek.text = "월"
                 isMonth = !isMonth
+                setMonthCalendar(year, month)
             }
         }
     }
