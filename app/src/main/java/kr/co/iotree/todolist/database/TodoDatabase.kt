1c51bc597b8c6058e508d762eeaa967db41a09a3
jshong <jshong@iotree.co.kr>
2021-12-21 16:15:06 +0900

    viewmodel

--- app/src/main/java/kr/co/iotree/todolist/database/TodoDatabase.kt
+++ app/src/main/java/kr/co/iotree/todolist/database/TodoDatabase.kt
@@ -1,7 +1,6 @@
 package kr.co.iotree.todolist.database
 
 import android.content.Context
-import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
